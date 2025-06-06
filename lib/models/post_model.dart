962152c4cf0393c6e6ce9e3ac0a467ab93658245
kusha2000 <kushanandarawewa1@gmail.com>
2025-06-06 09:30:57 +0530

    Create the Models

---
+++ lib/models/post_model.dart
@@ -0,0 +1,56 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:social_connect/utils/util_functions/mood.dart';
+
+class Post {
+  final String postCaption;
+  final Mood mood;
+  final String userId;
+  final String username;
+  final int likes;
+  final String postId;
+  final DateTime datePublished;
+  final String postUrl;
+  final String profImage;
+
+  Post({
+    required this.postCaption,
+    required this.mood,
+    required this.userId,
+    required this.username,
+    required this.likes,
+    required this.postId,
+    required this.datePublished,
+    required this.postUrl,
+    required this.profImage,
+  });
+
+  // Convert a Post instance to a map (for saving to Firestore)
+  Map<String, dynamic> toJson() {
+    return {
+      'postCaption': postCaption,
+      'mood': mood.name, // Use enum name
+      'userId': userId,
+      'username': username,
+      'likes': likes,
+      'postId': postId,
+      'datePublished': Timestamp.fromDate(datePublished),
+      'postUrl': postUrl,
+      'profImage': profImage,
+    };
+  }
+
+  // Create a Post instance from a map (for retrieving from Firestore)
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      postCaption: json['postCaption'] ?? '',
+      mood: MoodExtension.fromString(json['mood'] ?? 'happy'),
+      userId: json['userId'] ?? '',
+      username: json['username'] ?? '',
+      likes: json['likes'] ?? 0,
+      postId: json['postId'] ?? '',
+      datePublished: (json['datePublished'] as Timestamp).toDate(),
+      postUrl: json['postUrl'] ?? '',
+      profImage: json['profImage'] ?? '',
+    );
+  }
+}
---
+++ lib/models/user_model.dart
@@ -0,0 +1,55 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserModel {
+  final String userId;
+  final String name;
+  final String email;
+  final String jobTitle;
+  final String imageUrl;
+  final DateTime createdAt;
+  final DateTime updatedAt;
+  final String password;
+  final int followers;
+
+  UserModel({
+    required this.userId,
+    required this.name,
+    required this.email,
+    required this.jobTitle,
+    required this.imageUrl,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.password,
+    required this.followers,
+  });
+
+  // Convert a User instance to a map (for saving to Firestore)
+  Map<String, dynamic> toMap() {
+    return {
+      'userId': userId,
+      'name': name,
+      'email': email,
+      'jobTitle': jobTitle,
+      'imageUrl': imageUrl,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
+      'password': password,
+      'followers': followers,
+    };
+  }
+
+  // Create a User instance from a map (for retrieving from Firestore)
+  factory UserModel.fromMap(Map<String, dynamic> map) {
+    return UserModel(
+      userId: map['userId'] ?? '',
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      jobTitle: map['jobTitle'] ?? '',
+      imageUrl: map['imageUrl'] ?? '',
+      createdAt: (map['createdAt'] as Timestamp).toDate(),
+      updatedAt: (map['updatedAt'] as Timestamp).toDate(),
+      password: map['password'] ?? '',
+      followers: map['followers'] ?? 0,
+    );
+  }
+}
