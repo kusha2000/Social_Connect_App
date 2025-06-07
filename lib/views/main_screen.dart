bb1ac149799798074018e99569ab58903688dfed
kusha2000 <kushanandarawewa1@gmail.com>
2025-06-07 08:59:55 +0530

    Create The main Bottom Navigation Bar

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -1,18 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:social_connect/services/auth/auth_service.dart';
+import 'package:social_connect/views/main_views/create_post_screen.dart';
+import 'package:social_connect/views/main_views/feed_screen.dart';
+import 'package:social_connect/views/main_views/profile_screen.dart';
+import 'package:social_connect/views/main_views/reels_screen.dart';
+import 'package:social_connect/views/main_views/search_screen.dart';
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    FeedScreen(),
+    SearchScreen(),
+    CreatePostScreen(),
+    ReelsScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () async {
-              await AuthService().signOut();
-            },
-            child: Text("Sign Out")),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle_outline),
+            label: 'Create',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.video_library),
+            label: 'Reels',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _currentIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
---
+++ lib/views/main_views/create_post_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CreatePostScreen extends StatefulWidget {
+  const CreatePostScreen({super.key});
+
+  @override
+  State<CreatePostScreen> createState() => _CreatePostScreenState();
+}
+
+class _CreatePostScreenState extends State<CreatePostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Create Post Screen"),
+    );
+  }
+}
